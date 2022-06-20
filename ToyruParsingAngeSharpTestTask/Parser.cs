2aba9105f7444d44ac37846f12b85c0e6d7856cd
VerKarp <vrkrpv@gmail.com>
2022-06-20 14:49:39 +0300

    Adding a class to collect information about a toy

---
+++ ToyruParsingAngeSharpTestTask/Parser.cs
@@ -0,0 +1,17 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyruParsingAngeSharpTestTask
+{
+    class Parser
+    {
+        const string address = "https://www.toy.ru/catalog/boy_transport/";
+        readonly IConfiguration config = Configuration.Default.WithDefaultLoader();
+
+        public List<ToyInfo> Toys { get; set; } = new();
+    }
+}
