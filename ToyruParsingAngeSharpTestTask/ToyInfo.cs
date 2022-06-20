6ae47532f7ed089160ebff6ae66b04dbd8d53409
VerKarp <vrkrpv@gmail.com>
2022-06-20 14:44:56 +0300

    Adding a class to collect information about a toy

---
+++ ToyruParsingAngeSharpTestTask/ToyInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ToyruParsingAngeSharpTestTask
+{
+    class ToyInfo
+    {
+        public string RegionName { get; set; }
+        public string Breadcrumbs { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string OldPrice { get; set; }
+        public string Availability { get; set; }
+        public string Images { get; set; }
+        public string Link { get; set; }
+    }
+}
